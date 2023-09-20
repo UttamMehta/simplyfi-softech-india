b6d69d336daffe29b0a06b18056ad655c2131641
unknown <binobajichiku@gmail.com>
2023-09-20 15:55:00 +0530

    added dsa

--- second assignment/index.js
+++ second assignment/index.js
@@ -1,59 +1,39 @@
-
-function findRoute(tickets) {
-    const visited = new Set();
-    const route = [];
-  
-    function dfs(city) {
-      if (city === 'Amsterdam') {
-        // Destination reached
-        return true;
-      }
-  
-      if (visited.has(city)) {
-        // Avoid revisiting cities
-        return false;
-      }
-  
-      visited.add(city);
-      route.push(city);
-  
-      if (city in tickets) {
-        const destinations = tickets[city];
-        for (const destination of destinations) {
-          if (dfs(destination)) {
-            return true;
-          }
-        }
-      }
-  
-      // If we reach this point, backtrack
-      route.pop();
-      visited.delete(city);
-      return false;
-    }
-  
-    if (dfs('Kiev')) {
-      // A valid route was found
-      return route;
-    } else {
-      // No valid route from Kiev to Amsterdam
+function findRoute(tickets, citiesVisited) {
+    if (citiesVisited.length === 0) {
       return [];
     }
+  
+    const route = [citiesVisited[0]]; // Start with the first city
+  
+    for (let i = 1; i < citiesVisited.length; i++) {
+      const fromCity = citiesVisited[i - 1];
+      const toCity = citiesVisited[i];
+  
+      if (tickets[fromCity] && tickets[fromCity].includes(toCity)) {
+        route.push(toCity); // Add the destination to the route
+      } else {
+        // Invalid route, return an empty array
+        return [];
+      }
+    }
+  
+    return route;
   }
   
   const availableTickets = {
-    'Paris': 'Skopje',
-    'Zurich': 'Amsterdam',
-    'Prague': 'Zurich',
-    'Barcelona': 'Berlin',
-    'Kiev': 'Prague',
-    'Skopje': 'Paris',
-    'Amsterdam': 'Barcelona',
-    'Berlin': 'Kiev',
-    'Berlin': 'Amsterdam',
+    'Paris': ['Skopje'],
+    'Zurich': ['Amsterdam'],
+    'Prague': ['Zurich'],
+    'Barcelona': ['Berlin'],
+    'Kiev': ['Prague'],
+    'Skopje': ['Paris'],
+    'Amsterdam': ['Barcelona', 'Berlin'],
+    'Berlin': ['Kiev', 'Amsterdam'],
   };
   
-  const route = findRoute(availableTickets);
+  const citiesVisited = ['Kiev', 'Prague', 'Zurich', 'Amsterdam', 'Barcelona', 'Berlin'];
+  
+  const route = findRoute(availableTickets, citiesVisited);
   
   if (route.length > 0) {
     console.log('Your son traveled to the following cities:');
